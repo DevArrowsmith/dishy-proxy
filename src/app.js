c5b1fe268edc7bc67b887a66ac8ca155c35a2995
devarrowsmith <devarrowsmith@gmail.com>
2021-09-14 21:40:22 +0100

    Update API: add an endpoint to fetch a business by id.

--- src/app.js
+++ src/app.js
@@ -10,10 +10,30 @@ app.use(cors());
 
 app.get('/restaurants/:location', async (req, res) => {
   const location = req.params.location;
-  console.log(location);
   try{
     const response = await fetch(
       `https://api.yelp.com/v3/businesses/search?radius=2000&location=${location}&term=restaurants`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      }
+    );
+
+    const yelpData = await response.json();
+    res.status(200).json(yelpData);
+
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500)
+  }
+});
+
+app.get('/restaurant/:restaurantId', async (req, res) => {
+  const restaurantId = req.params.restaurantId;
+  try{
+    const response = await fetch(
+      `https://api.yelp.com/v3/businesses/${restaurantId}`, {
         method: 'GET',
         headers: {
           Authorization: `Bearer ${TOKEN}`,
